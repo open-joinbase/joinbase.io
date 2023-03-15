5f1f539443bcf1bd3576d4189c30d6af9fd3dd54
joinbase0 <joinbase0@joinbase.io>
2023-03-15 18:09:30 +0800

    pub

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// Set darkmode
+document.getElementById('mode').addEventListener('click', () => {
+
+  document.body.classList.toggle('dark');
+  localStorage.setItem('theme', document.body.classList.contains('dark') ? 'dark' : 'light');
+
+});
+
+// enforce local storage setting but also fallback to user-agent preferences
+if (localStorage.getItem('theme') === 'dark' || (!localStorage.getItem('theme') && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+
+  document.body.classList.add('dark');
+
+}
+
+// enforce local storage setting but also fallback to user-agent preferences
+if (localStorage.getItem('theme') === 'light' || (!localStorage.getItem('theme') && window.matchMedia("(prefers-color-scheme: light)").matches)) {
+
+  document.body.classList.add('light');
+
+}
